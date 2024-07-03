f7624f31fa847b7103788fcdd0b76007f953d877
Gustavo Luis <100378668+Gustavoluisslima@users.noreply.github.com>
2024-07-03 16:49:45 -0300

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,3 @@
-/*function somaValores() {
-    const valor1 = Number(prompt("informe o valor"));
-    const valor2 = Number(prompt("Informe o valor"));
-    
-    return valor1 + valor2;
-}
-
-const resultado = somaValores();
-alert(`resultado:${resultado}`);*/
-
 // Algoritmo "ParOuImpar"
 function verificarParOuImpar(numero) {
     if (numero % 2 === 0) {
